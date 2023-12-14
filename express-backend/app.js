30b8075bac946d4ae2f2f1b658595d3e9086dc72
kol3x <kolaegipet@gmail.com>
2023-12-14 12:48:55 +0200

    same import fix for backend

--- express-backend/app.js
+++ express-backend/app.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 const indexRouter = require("./routes/index");
+const { FRONTENDURL } = require("../config/index");
 
 
 const app = express();
